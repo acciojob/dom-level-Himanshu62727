9f7fbd88d0ca69be0f6e7009ee8d9eb5dae98dc6
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-10-04 10:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let output = document.querySelector("#level");
+
+let lvl = 0;
+while (element) {
+	lvl++
+	element = element.parseElement;
+}
+
+alert('The level of the element is' + lvl)
